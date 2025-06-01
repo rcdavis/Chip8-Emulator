0da10b3516c474d0c446606b113422d51fd08ea1
Ren Davis <rcdavisgd@gmail.com>
2025-06-01 07:54:32 -0400

    Added defines to intellisense

--- Chip8-Emulator/src/Application.cpp
+++ Chip8-Emulator/src/Application.cpp
@@ -5,9 +5,8 @@
 #include "ImGuiWindows/Chip8InfoWindow.h"
 #include "ImGuiWindows/OpcodeLogWindow.h"
 
-#define GLFW_INCLUDE_NONE
-#include <GLFW/glfw3.h>
 #include <glad/gl.h>
+#include <GLFW/glfw3.h>
 
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
