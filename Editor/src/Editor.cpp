b0c440358b1ce3fb84ff2264a87b923031ad5292
Ren Davis <rcdavisgd@gmail.com>
2025-12-10 11:19:16 -0500

    Added GLAD

--- Editor/src/Editor.cpp
+++ Editor/src/Editor.cpp
@@ -2,9 +2,12 @@
 
 #include <iostream>
 
+#include "glad/gl.h"
 #include "GLFW/glfw3.h"
 
 static void GlfwErrorCallback(int error, const char* description);
+static void APIENTRY GLDebugCallback(GLenum source, GLenum type, GLuint id, GLenum severity,
+	GLsizei length, const GLchar* message, const void* userParam);
 
 Editor::Editor() :
 	mWindow(nullptr)
@@ -27,6 +30,10 @@ bool Editor::Init() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+#ifdef DEBUG
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+#endif
+
 	mWindow = glfwCreateWindow(1600, 900, "Chip8 Emulator", nullptr, nullptr);
 	if (!mWindow) {
 		std::cerr << "Failed to create GLFW window\n";
@@ -35,7 +42,40 @@ bool Editor::Init() {
 
 	glfwMakeContextCurrent(mWindow);
 
+	if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) {
+		std::cerr << "Failed to init GLAD\n";
+		return false;
+	}
+
+#ifdef DEBUG
+	GLint glMajorVer = 0, glMinorVer = 0;
+	glGetIntegerv(GL_MAJOR_VERSION, &glMajorVer);
+	glGetIntegerv(GL_MINOR_VERSION, &glMinorVer);
+	std::cout << "OpenGL Info:\n";
+	std::cout << "  Vendor: " << glGetString(GL_VENDOR) << "\n";
+	std::cout << "  Renderer: " << glGetString(GL_RENDERER) << "\n";
+	std::cout << "  Version: " << glMajorVer << "." << glMinorVer << "\n";
+
+	//LOG_INFO("OpenGL Info:");
+	//LOG_INFO("  Vendor: {0}", (char*)glGetString(GL_VENDOR));
+	//LOG_INFO("  Renderer: {0}", (char*)glGetString(GL_RENDERER));
+	//LOG_INFO("  Version: {0}.{1}", glMajorVer, glMinorVer);
+
+	if (GLAD_GL_KHR_debug) {
+		std::cout << "Setting up OpenGL debug callback...\n";
+		//LOG_INFO("Setting up OpenGL debug callback...");
+		glEnable(GL_DEBUG_OUTPUT);
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+		glDebugMessageCallback(GLDebugCallback, nullptr);
+	} else {
+		std::cout << "Unable to use OpenGL debug callback\n";
+		//LOG_INFO("Unable to use OpenGL debug callback");
+	}
+#endif
+
 	glfwSwapInterval(1);
+
+	glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
 
 	return true;
 }
@@ -44,6 +84,8 @@ void Editor::Run() {
 	while (!glfwWindowShouldClose(mWindow)) {
 		glfwPollEvents();
 
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers(mWindow);
 	}
 }
@@ -51,4 +93,10 @@ void Editor::Run() {
 void GlfwErrorCallback(int error, const char* description) {
 	std::cerr << "GLFW Error (" << error << "): " << description << "\n";
 	//LOG_ERROR("GLFW Error ({0}): {1}", error, description);
+}
+
+void APIENTRY GLDebugCallback(GLenum source, GLenum type, GLuint id, GLenum severity,
+	GLsizei length, const GLchar* message, const void* userParam)
+{
+	std::cerr << "OpenGL Debug Message: " << message << "\n";
 }
